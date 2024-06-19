7b42fbe479f03cceaebf6a4d929c546ccc7c2096
Bharat Kasera <bharat.codewolf@gmail.com>
2024-06-19 18:01:40 +0530

    updates

---
+++ README.md
@@ -0,0 +1,22 @@
+# CaseCobra - A Modern Fullstack E-Commerce Shop for Custom Phone Cases
+
+Built with the Next.js 14 App Router, Postgres, TypeScript, Tailwind & Kinde Auth
+
+![Project Image](https://github.com/Bharat-kasera/casecobra/blob/main/public/thumbnail.png)
+
+## Features
+
+- 🛠️ Complete shop built from scratch in Next.js 14
+- 💻 Beautiful landing page included
+- 🎨 Custom artworks made by a professional illustrator
+- 💳 Secret admin dashboard to manage orders
+- 🖥️ Drag-and-drop file uploads
+- 🛍️ Customers can purchase directly from you
+- 🌟 Clean, modern UI on top of shadcn-ui
+- 🛒 Completely custom phone case configurator
+- 🔑 Authentication using Kinde
+- ✉️ Beautiful thank-you email after purchase
+- ✅ Apple-inspired configuration design
+- ⌨️ 100% written in TypeScript
+- 🎁 ...much more
+
---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from 'next';
+import { Recursive } from 'next/font/google';
+import './globals.css';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import { Toaster } from '@/components/ui/toaster';
+import Providers from '@/components/Providers';
+import { constructMetadata } from '@/lib/utils';
+import NextTopLoader from 'nextjs-toploader';
+const recursive = Recursive({ subsets: ['latin'] });
+
+export const metadata = constructMetadata();
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={recursive.className}>
+      <NextTopLoader color="#00ECC1"  />
+        <Navbar />
+        <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
+          <div className="flex-1 flex flex-col h-full">
+            <Providers>{children}</Providers>
+          </div>
+          <Footer />
+        </main>
+        <Toaster />
+      </body>
+    </html>
+  );
+}
